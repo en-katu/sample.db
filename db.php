eefee1f4b7989d68f0bfcd2369d0a80bc0919b4d
enkatu <79071721+en-katu@users.noreply.github.com>
2024-02-21 00:57:32 +0900

    Add files via upload

---
+++ db.php
@@ -0,0 +1,31 @@
+<?php
+$db = new PDO("sqlite:sample.db");
+$res=$db->query("select * from users");
+//foreach($res as $value){
+  //  var_dump($value);
+//}
+$db =null;
+//php -s localhost:80080
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table border="1">
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+        </tr>
+        <?php foreach($res as $value):?>
+        <tr>
+            <td><?="$value[id];"?></td>
+            <td><?="$value[name];"?></td>
+        </tr>
+        <?php endforeach;?>
+    </table>
+</body>
+</html>
